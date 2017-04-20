80492b100015dc47c77d1ea3b7fc4c40b68e2bcd
Paul Shannon <paul@sitepen.com>
2017-04-19 17:15:59 -0700

    added tests

---
+++ tests/intern.ts
@@ -0,0 +1,20 @@
+import 'intern';
+
+export const loaderOptions = {
+	packages: [
+		{ name: 'tslib', location: './node_modules/tslib', main: 'tslib.js' },
+		{ name: 'tasks', location: './_build/tasks' },
+		{ name: 'src', location: './_build/src' },
+		{ name: 'tests', location: './_build/tests' }
+	]
+};
+
+export const suites = [ 'tests/unit/all' ];
+
+export const excludeInstrumentation = /^(?:_build\/tests|node_modules)\//;
+
+export const loaders = {
+	'host-node': '@dojo/loader'
+};
+
+export const filterErrorStack = true;
