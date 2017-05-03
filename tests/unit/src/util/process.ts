d48658c29a771406f182f2f644af2fc7995596dc
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-05-03 15:20:12 -0500

    make a simpler stub for promisify tests

--- tests/unit/src/util/process.ts
+++ tests/unit/src/util/process.ts
@@ -1,53 +1,26 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
 import * as process from '../../../../src/util/process';
-import loadModule, { cleanupModuleMocks } from '../../../_support/loadModule';
-import { createStubInstance, stub, SinonStub } from 'sinon';
-import { ChildProcess } from 'child_process';
-
-let module: any;
-let execStub: SinonStub;
-let spawnStub: SinonStub;
-let ChildProcessStub: SinonStub;
-let ExecOptionsStub: SinonStub;
-let SpawnOptionsStub: SinonStub;
+import { stub } from 'sinon';
 
 registerSuite({
 	name: 'util/process',
 
-	before() {
-		execStub = stub();
-		spawnStub = stub();
-		ChildProcessStub = createStubInstance(ChildProcess);
-		ExecOptionsStub = stub();
-		SpawnOptionsStub = stub();
-	},
-
-	after() {
-		cleanupModuleMocks();
-	},
-
-	beforeEach() {
-		module = loadModule('src/util/process', {
-			exec: execStub,
-			spawn: spawnStub,
-			ChildProcess: ChildProcessStub,
-			ExecOptions: ExecOptionsStub,
-			SpawnOptions: SpawnOptionsStub
-		}, false);
-	},
-
-	afterEach() {
-		execStub.reset();
-		spawnStub.reset();
-		ChildProcessStub.reset();
-		ExecOptionsStub.reset();
-		SpawnOptionsStub.reset();
-	},
-
 	async promisify() {
-		const proc = execStub()
-		process.promisify()
-	},
-});
+		const proc = {
+			on: stub()
+		};
+		const prom = await process.promisify(proc);
+		const closeCallback = proc.on.lastCall.args[1];
 
+		assert.strictEqual(proc.on.lastCall.args[0], 'close');
+
+		closeCallback(0);
+
+		assert.strictEqual(prom, proc);
+
+		closeCallback(1);
+
+		assert.throws(prom);
+	}
+});
