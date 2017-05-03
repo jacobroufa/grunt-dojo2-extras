4756f07fedb8522c9db44bff33c81153ebebdbe2
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-05-03 14:01:42 -0500

    WIP stubbing out process

--- tests/unit/src/util/process.ts
+++ tests/unit/src/util/process.ts
@@ -2,28 +2,52 @@ import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
 import * as process from '../../../../src/util/process';
 import loadModule, { cleanupModuleMocks } from '../../../_support/loadModule';
-import { stub, SinonStub } from 'sinon';
+import { createStubInstance, stub, SinonStub } from 'sinon';
+import { ChildProcess } from 'child_process';
 
 let module: any;
-let execChild: SinonStub;
-let spawnChild: SinonStub;
-let ChildProcess: SinonStub;
-let ChildExecOptions: SinonStub;
-let ChildSpawnOptions: SinonStub;
+let execStub: SinonStub;
+let spawnStub: SinonStub;
+let ChildProcessStub: SinonStub;
+let ExecOptionsStub: SinonStub;
+let SpawnOptionsStub: SinonStub;
 
 registerSuite({
 	name: 'util/process',
 
 	before() {
+		execStub = stub();
+		spawnStub = stub();
+		ChildProcessStub = createStubInstance(ChildProcess);
+		ExecOptionsStub = stub();
+		SpawnOptionsStub = stub();
 	},
 
 	after() {
+		cleanupModuleMocks();
 	},
 
 	beforeEach() {
+		module = loadModule('src/util/process', {
+			exec: execStub,
+			spawn: spawnStub,
+			ChildProcess: ChildProcessStub,
+			ExecOptions: ExecOptionsStub,
+			SpawnOptions: SpawnOptionsStub
+		}, false);
 	},
 
 	afterEach() {
-	}
+		execStub.reset();
+		spawnStub.reset();
+		ChildProcessStub.reset();
+		ExecOptionsStub.reset();
+		SpawnOptionsStub.reset();
+	},
+
+	async promisify() {
+		const proc = execStub()
+		process.promisify()
+	},
 });
 
