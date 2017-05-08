1417969fc8b7e28532a48d286843207a4f61c523
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-05-08 15:38:18 -0500

    add complete exec and spawn tests

--- tests/unit/src/util/process.ts
+++ tests/unit/src/util/process.ts
@@ -2,13 +2,12 @@ import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
 import { stub, SinonStub } from 'sinon';
 import loadModule, { cleanupModuleMocks } from '../../../_support/loadModule';
-import * as process from '../../../../src/util/process';
+import * as process from 'src/util/process';
+import { LogStream } from 'src/log';
 
 let module: any;
 let execStub: SinonStub;
 let spawnStub: SinonStub;
-let loggerStub: SinonStub;
-let LogStreamStub: SinonStub;
 
 registerSuite({
 	name: 'util/process',
@@ -16,8 +15,6 @@ registerSuite({
 	before() {
 		execStub = stub();
 		spawnStub = stub();
-		loggerStub = stub();
-		LogStreamStub = stub();
 	},
 
 	after() {
@@ -29,12 +26,6 @@ registerSuite({
 			child_process: {
 				exec: execStub,
 				spawn: spawnStub
-			},
-			'../log': {
-				logger: {
-					debug: loggerStub
-				},
-				LogStream: LogStreamStub
 			}
 		});
 	},
@@ -42,60 +33,101 @@ registerSuite({
 	afterEach() {
 		execStub.reset();
 		spawnStub.reset();
-		loggerStub.reset();
-		LogStreamStub.reset();
 	},
 
-	promisify() {
-		process.promisify(process.exec('ls', { silent: true })).then((procVal) => {
-			assert.isOk(procVal);
-		});
+	promisify: {
+		'promisify eventually resolves the returned promise'() {
+			process.promisify(process.exec('ls', { silent: true })).then((procVal) => {
+				assert.isOk(procVal);
+			});
+		},
 
-		process.promisify(process.exec('not-a-command', { silent: true })).then(() => {
-			assert.fail();
-		}, (e) => {
-			assert.instanceOf(e, Error);
-		});
+		'if child process has exit code other than 0, promisify eventually rejects the returned promise'() {
+			process.promisify(process.exec('not-a-command', { silent: true })).then(() => {
+				assert.fail();
+			}, (e) => {
+				assert.instanceOf(e, Error);
+			});
+		}
 	},
 
-	exec() {
-		let value = 'ls';
+	exec: {
+		'execChild is called, the return value of which is returned; options not applied'() {
+			let value = 'ls';
 
-		execStub.returns(value);
+			execStub.returns(value);
 
-		let proc = module.exec(value, { silent: true });
+			let proc = module.exec(value, { silent: true });
 
-		assert.isTrue(loggerStub.calledOnce);
-		assert.isTrue(loggerStub.calledWith('exec ls'));
-		assert.isTrue(execStub.calledOnce);
-		assert.isTrue(LogStreamStub.notCalled);
-		assert.strictEqual(proc, value);
+			assert.isTrue(execStub.calledOnce);
+			assert.strictEqual(proc, value);
+		},
 
-		execStub.returns({
-			stdout: { pipe: stub() },
-			stderr: { pipe: stub() }
-		});
+		'options applied after execChild is called'() {
+			// required for the internal named function `applyOptions`
+			execStub.returns({
+				stdout: { pipe: stub() },
+				stderr: { pipe: stub() }
+			});
 
-		module.exec(value, { silent: false });
-		assert.isTrue(LogStreamStub.calledTwice);
+			const proc = module.exec('ls', { silent: false });
+
+			assert.isTrue(proc.stdout.pipe.calledOnce);
+			assert.isTrue(proc.stderr.pipe.calledOnce);
+			assert.instanceOf(proc.stdout.pipe.lastCall.args[0], LogStream);
+			assert.instanceOf(proc.stderr.pipe.lastCall.args[0], LogStream);
+		}
 	},
 
-	async promiseExec() {
-		module.promiseExec('ls', { silent: true }).then((procVal: any) => {
-			assert.isOk(procVal);
-		});
+	promiseExec: {
+		// 'returns exec wrapped in promise, called with specified options'() {
+		// 	const command = 'ls';
+		// 	const opts = { silent: true };
+		// 	const promisifySpy = spy(process, 'promisify');
+		// 	const processExecSpy = spy(process, 'exec');
+		// 	const execPromise = module.promiseExec(command, opts);
 
-		module.promiseExec('not-a-command', { silent: true }).then(() => {
-			assert.fail();
-		}, (e: Error) => {
-			assert.instanceOf(e, Error);
-		});
+		// 	assert.instanceOf(execPromise, Promise);
+		// 	assert.isTrue(promisifySpy.calledOnce);
+		// 	assert.isTrue(processExecSpy.calledOnce);
+		// 	assert.isTrue(processExecSpy.calledWith(command, opts));
+		// },
+
+		// 'sets options.silent to false if unavailable'() {
+		// 	const processExecStub = stub(module, 'exec');
+
+		// 	module.promiseExec('not-a-command', {});
+
+		// 	assert.isTrue(processExecStub.calledWith('not-a-command', { silent: false }));
+		// }
 	},
 
-	spawn() {
-		// stub logger.debug and assert calledOnce
-		// stub spawnChild and assert calledOnce
-		// sutb applyOptions? and assert calledOnce
+	spawn: {
+		'spawnChild is called, the return value of which is returned; options not applied'() {
+			let value = 'ls';
+
+			spawnStub.returns(value);
+
+			let proc = module.spawn(value, [ '-l' ], { silent: true });
+
+			assert.isTrue(spawnStub.calledOnce);
+			assert.strictEqual(proc, value);
+		},
+
+		'options applied after spawnChild is called'() {
+			// required for the internal named function `applyOptions`
+			spawnStub.returns({
+				stdout: { pipe: stub() },
+				stderr: { pipe: stub() }
+			});
+
+			const proc = module.spawn('ls', [ '-l' ], { silent: false });
+
+			assert.isTrue(proc.stdout.pipe.calledOnce);
+			assert.isTrue(proc.stderr.pipe.calledOnce);
+			assert.instanceOf(proc.stdout.pipe.lastCall.args[0], LogStream);
+			assert.instanceOf(proc.stderr.pipe.lastCall.args[0], LogStream);
+		}
 	},
 
 	promiseSpawn() {
