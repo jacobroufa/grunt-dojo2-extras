3ebd86b2c2ef6d7313688b4a9e085c78d13ffd00
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-05-08 11:16:58 -0500

    more WIP process tests

--- tests/unit/src/util/process.ts
+++ tests/unit/src/util/process.ts
@@ -1,26 +1,106 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
+import { stub, SinonStub } from 'sinon';
+import loadModule, { cleanupModuleMocks } from '../../../_support/loadModule';
 import * as process from '../../../../src/util/process';
-import { stub } from 'sinon';
+
+let module: any;
+let execStub: SinonStub;
+let spawnStub: SinonStub;
+let loggerStub: SinonStub;
+let LogStreamStub: SinonStub;
 
 registerSuite({
 	name: 'util/process',
 
-	async promisify() {
-		const proc = {
-			on: stub()
-		};
-		const prom = await process.promisify(proc);
-		const closeCallback = proc.on.lastCall.args[1];
+	before() {
+		execStub = stub();
+		spawnStub = stub();
+		loggerStub = stub();
+		LogStreamStub = stub();
+	},
 
-		assert.strictEqual(proc.on.lastCall.args[0], 'close');
+	after() {
+		cleanupModuleMocks();
+	},
 
-		closeCallback(0);
+	beforeEach() {
+		module = loadModule('src/util/process', {
+			child_process: {
+				exec: execStub,
+				spawn: spawnStub
+			},
+			'../log': {
+				logger: {
+					debug: loggerStub
+				},
+				LogStream: LogStreamStub
+			}
+		});
+	},
 
-		assert.strictEqual(prom, proc);
+	afterEach() {
+		execStub.reset();
+		spawnStub.reset();
+		loggerStub.reset();
+		LogStreamStub.reset();
+	},
 
-		closeCallback(1);
+	promisify() {
+		process.promisify(process.exec('ls', { silent: true })).then((procVal) => {
+			assert.isOk(procVal);
+		});
 
-		assert.throws(prom);
+		process.promisify(process.exec('not-a-command', { silent: true })).then(() => {
+			assert.fail();
+		}, (e) => {
+			assert.instanceOf(e, Error);
+		});
+	},
+
+	exec() {
+		let value = 'ls';
+
+		execStub.returns(value);
+
+		let proc = module.exec(value, { silent: true });
+
+		assert.isTrue(loggerStub.calledOnce);
+		assert.isTrue(loggerStub.calledWith('exec ls'));
+		assert.isTrue(execStub.calledOnce);
+		assert.isTrue(LogStreamStub.notCalled);
+		assert.strictEqual(proc, value);
+
+		execStub.returns({
+			stdout: { pipe: stub() },
+			stderr: { pipe: stub() }
+		});
+
+		module.exec(value, { silent: false });
+		assert.isTrue(LogStreamStub.calledTwice);
+	},
+
+	async promiseExec() {
+		module.promiseExec('ls', { silent: true }).then((procVal: any) => {
+			assert.isOk(procVal);
+		});
+
+		module.promiseExec('not-a-command', { silent: true }).then(() => {
+			assert.fail();
+		}, (e: Error) => {
+			assert.instanceOf(e, Error);
+		});
+	},
+
+	spawn() {
+		// stub logger.debug and assert calledOnce
+		// stub spawnChild and assert calledOnce
+		// sutb applyOptions? and assert calledOnce
+	},
+
+	promiseSpawn() {
+		// stub promisify and assert called once
+		// stub spawn and assert called once
+		// assert return is wrapped in promise
 	}
 });
