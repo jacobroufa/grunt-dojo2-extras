4bc28008f78ed18aecd195feb91f2d5c1533c726
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-05-10 20:17:26 -0500

    finish tests for `commands/publish`

--- tests/unit/src/commands/publish.ts
+++ tests/unit/src/commands/publish.ts
@@ -4,26 +4,13 @@ import loadModule, { cleanupModuleMocks } from '../../../_support/loadModule';
 import { stub, SinonStub } from 'sinon';
 
 let publish: any;
-let Git: any;
 let gitCommitStub: SinonStub;
-let loggerStub: SinonStub;
 
 registerSuite({
 	name: 'commands/publish',
 
 	before() {
-		Git = class {
-			static cloneDirectory = 'dir';
-
-			static getConfig: SinonStub = stub();
-			static areFilesChanged: SinonStub = stub();
-			static ensureConfig: SinonStub = stub();
-			static add: SinonStub = stub();
-			static commit: SinonStub = stub();
-			static push: SinonStub = stub();
-		};
 		gitCommitStub = stub();
-		loggerStub = stub();
 	},
 
 	after() {
@@ -32,21 +19,14 @@ registerSuite({
 
 	beforeEach() {
 		publish = loadModule('src/commands/publish', {
-			'../util/Git': { default: Git },
 			'../util/environment': {
 				gitCommit: gitCommitStub
-			},
-			'../log': {
-				logger: {
-					info: loggerStub
-				}
 			}
 		});
 	},
 
 	afterEach() {
 		gitCommitStub.reset();
-		loggerStub.reset();
 	},
 
 	'publish': {
@@ -54,39 +34,82 @@ registerSuite({
 			const opts = {
 				branch: 'master',
 				publishMode: stub().returns('skip'),
-				repo: Git
+				repo: {
+					areFilesChanged: stub()
+				}
 			};
 
 			await publish(opts);
 
 			assert.isTrue(opts.publishMode.calledOnce);
-			assert.strictEqual(loggerStub.lastCall.args[0], 'skipping publish.');
+			assert.isTrue(opts.repo.areFilesChanged.notCalled);
 		},
 
 		'publishMode is a string': (() => {
-			const opts = {
-				branch: 'master',
-				publishMode: 'publish',
-				repo: Git
-			};
+			let opts: any;
 
 			return {
+				beforeEach() {
+					opts = {
+						branch: 'master',
+						publishMode: 'commit',
+						repo: {
+							add: stub().returns(Promise.resolve()),
+							areFilesChanged: stub().returns(Promise.resolve(true)),
+							commit: stub().returns(Promise.resolve()),
+							ensureConfig: stub().returns(Promise.resolve()),
+							getConfig: stub().returns(Promise.resolve('username')),
+							push: stub().returns(Promise.resolve())
+						}
+					};
+				},
+
+				afterEach() {
+					opts.repo.add.reset();
+					opts.repo.areFilesChanged.reset();
+					opts.repo.commit.reset();
+					opts.repo.ensureConfig.reset();
+					opts.repo.getConfig.reset();
+					opts.repo.push.reset();
+				},
+
 				async 'repo has no changes'() {
-					Git.areFilesChanged.returns(false);
+					const areFilesChanged = opts.repo.areFilesChanged;
+
+					areFilesChanged.returns(Promise.resolve(false));
 
 					await publish(opts);
 
-					assert.isTrue(Git.areFilesChanged.calledOnce);
-					assert.strictEqual(loggerStub.lastCall.args[0], 'No files changed. Skipping publish.');
+					assert.isTrue(areFilesChanged.calledOnce);
+					assert.isTrue(opts.repo.ensureConfig.notCalled);
 				},
-				'publishMode is "publish"'() {
-				},
-				'publishMode is "commit"': {
-					'gitCommit returns true'() {
-					},
 
-					'gitCommit returns false'() {
-					}
+				async 'publishMode is "commit"; gitCommit returns value'() {
+					gitCommitStub.returns('a35de344');
+
+					await publish(opts);
+
+					assert.isTrue(opts.repo.ensureConfig.calledOnce);
+					assert.isTrue(opts.repo.add.calledOnce);
+					assert.isTrue(opts.repo.commit.calledOnce);
+					assert.isTrue(opts.repo.commit.calledWith('Published by username from commit a35de344'));
+					assert.isTrue(opts.repo.push.notCalled);
+				},
+
+				async 'publishMode is "commit"; gitCommit returns falsy'() {
+					gitCommitStub.returns(undefined);
+
+					await publish(opts);
+
+					assert.isTrue(opts.repo.commit.calledWith('Published by username'));
+				},
+
+				async 'publishMode is "publish"'() {
+					opts.publishMode = 'publish';
+
+					await publish(opts);
+
+					assert.isTrue(opts.repo.push.calledOnce);
 				}
 			};
 		})()
