a2eede063c2d6b95dd61be9aa6ac7b630a49f155
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-05-11 16:50:23 -0500

    WIP - decryptData not properly returning chained method

---
+++ tests/unit/src/commands/decryptDeployKey.ts
@@ -0,0 +1,102 @@
+import * as registerSuite from 'intern!object';
+import * as assert from 'intern/chai!assert';
+import loadModule, { cleanupModuleMocks } from '../../../_support/loadModule';
+import { stub, SinonStub } from 'sinon';
+
+let decryptDeployKey: any;
+let decryptDataObj: any;
+let decryptDataStub: SinonStub;
+let encryptedKeyFileStub: SinonStub;
+let keyFileStub: SinonStub;
+let existsSyncStub: SinonStub;
+let createWriteStreamStub: SinonStub;
+let createReadStreamStub: SinonStub;
+
+registerSuite({
+	name: 'commands/decryptDeployKey',
+
+	before() {
+		decryptDataObj = {
+			on: stub().returns(decryptDataObj),
+			pipe: stub().returns(decryptDataObj)
+		};
+		decryptDataStub = stub().returns(decryptDataObj);
+		encryptedKeyFileStub = stub();
+		keyFileStub = stub();
+		existsSyncStub = stub();
+		createWriteStreamStub = stub().returns('writeStream');
+		createReadStreamStub = stub().returns('readStream');
+	},
+
+	after() {
+		cleanupModuleMocks();
+	},
+
+	beforeEach() {
+		decryptDeployKey = loadModule('src/commands/decryptDeployKey', {
+			'../util/crypto': {
+				decryptData: decryptDataStub
+			},
+			'../util/environment': {
+				env: {
+					decryptKeyName: 'decryptKeyName',
+					decryptIvName: 'decryptIvName',
+					encryptedKeyFile: encryptedKeyFileStub,
+					keyFile: keyFileStub
+				}
+			},
+			'fs': {
+				existsSync: existsSyncStub,
+				createWriteStream: createWriteStreamStub,
+				createReadStream: createReadStreamStub
+			}
+		});
+	},
+
+	afterEach() {
+		decryptDataStub.reset();
+		encryptedKeyFileStub.reset();
+		keyFileStub.reset();
+		existsSyncStub.reset();
+		createWriteStreamStub.reset();
+		createReadStreamStub.reset();
+		decryptDataObj.on.reset();
+		decryptDataObj.pipe.reset();
+	},
+
+	'decryptDeployKey': {
+		async 'arguments passed in explicitly'() {
+			existsSyncStub.onCall(0).returns(true);
+			existsSyncStub.onCall(1).returns(false);
+			decryptDataObj.on.withArgs('close').yields();
+
+			const promise = decryptDeployKey('encrypted.file', 'decrypt key', 'decrypt iv', 'decrypted.file');
+
+			assert.isTrue(encryptedKeyFileStub.notCalled);
+			assert.isTrue(keyFileStub.notCalled);
+			assert.isTrue(existsSyncStub.calledTwice);
+			assert.instanceOf(promise, Promise);
+
+			assert.isTrue(await promise);
+
+			assert.isTrue(createReadStreamStub.calledOnce);
+			assert.isTrue(createWriteStreamStub.calledOnce);
+			assert.isTrue(decryptDataStub.calledOnce);
+			assert.isTrue(decryptDataStub.calledWith('readStream', 'decrypt key', 'decrypt iv'));
+
+			return promise;
+		},
+
+		'arguments obtained from default'() {
+		},
+
+		'nonexistent files and falsy arguments; eventually returns false'() {
+		},
+
+		'decryption eventually rejects'() {
+		},
+
+		'decryption eventually resolves'() {
+		}
+	}
+});
