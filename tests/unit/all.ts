c2b5cced649bfa66251e6c006e9fbc34f0828962
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-05-22 18:18:24 -0500

    add tests for `commands/sync` (#27)

--- tests/unit/all.ts
+++ tests/unit/all.ts
@@ -1,6 +1,7 @@
 import './src/log';
 import './src/commands/getReleases';
 import './src/commands/installDependencies';
+import './src/commands/sync';
 import './src/util/crypto';
 import './src/util/environment';
 import './src/util/file';
