17a3cf490aaeeb540ee058e2e7d56d0199775394
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-05-22 18:00:44 -0500

    add tests for `util/GitHub` (#30)

--- tests/unit/all.ts
+++ tests/unit/all.ts
@@ -1,8 +1,9 @@
 import './src/log';
 import './src/commands/getReleases';
 import './src/util/crypto';
-import './src/util/streams';
 import './src/util/environment';
 import './src/util/file';
 import './src/util/Git';
+import './src/util/GitHub';
+import './src/util/streams';
 import './tasks/util/getGithubSlug';
