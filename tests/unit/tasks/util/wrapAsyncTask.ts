d0cec5fb54770cc3497274152dd13724591562ec
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-05-22 11:34:57 -0500

    finish test for `tasks/util/wrapAsyncTask`
    change mocking strategy to provide only basic config and mock needed to
    run the `sync` task as opposed to creating a simple task in the test
    suite. because of the way Grunt works (loading config by string name),
    when mocking a task in the suite there are errors as the function for
    the task is never actually called.

--- tests/unit/tasks/util/wrapAsyncTask.ts
+++ tests/unit/tasks/util/wrapAsyncTask.ts
@@ -1,67 +1,57 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
-import IMultiTask = grunt.task.IMultiTask;
 import * as grunt from 'grunt';
 import { stub } from 'sinon';
 import { loadTasks, runGruntTask, unloadTasks } from '../../../_support/loadGrunt';
-import wrapAsyncTask from 'tasks/util/wrapAsyncTask';
 
-const asyncStub = stub();
-
-async function testTask(grunt: IGrunt) {
-	async function testMultiTask(this: IMultiTask<any>) {
-		return asyncStub().returns('testMultiTask');
-	}
-
-	grunt.registerMultiTask('task', wrapAsyncTask(testMultiTask));
-}
+const syncStub = stub();
+const getGithubSlugStub = stub();
 
 registerSuite({
 	name: 'tasks/util/wrapAsyncTask',
 
 	setup() {
 		grunt.initConfig({
-			testTask: {}
+			sync: { latest: { options: {
+				branch: 'latest',
+				cloneDirectory: 'cloneDirectory'
+			}}}
 		});
 
-		loadTasks();
+		loadTasks({
+			'../src/commands/sync': { default: syncStub },
+			'./util/getGithubSlug': { default: getGithubSlugStub }
+		});
 	},
 
 	teardown() {
 		unloadTasks();
 	},
 
-	beforeEach() {
-		asyncStub.reset();
+	afterEach() {
+		syncStub.reset();
+		getGithubSlugStub.reset();
 	},
 
-	'task eventually completes'() {
-		runGruntTask('testTask');
+	'task eventually completes'(this: any) {
+		getGithubSlugStub.returns({ name: 'name', owner: 'owner' });
+		syncStub.returns(Promise.resolve());
 
-		assert.isTrue(asyncStub.calledOnce);
+		const dfd = this.async();
 
-		// const grunt = gruntMock('task');
-		// const task = stub().withArgs(grunt).returns(Promise.resolve());
-		// const wrappedTask = wrapAsyncTask(task(grunt));
-
-		// // const taskStub: IMultiTask<any> = stub().withArgs('task').returns();
-		// // const asyncStub = stub();
-
-		// assert.isFunction(wrappedTask);
-
-		// wrappedTask(grunt);
-
-		// assert.isTrue(asyncStub.calledOnce);
-		// assert.isTrue(asyncStub.neverCalledWith(false));
+		runGruntTask('sync:latest', dfd.callback((result: any) => {
+			assert.isUndefined(result);
+		}));
 	},
 
-	'task eventually rejects'() {
+	'task eventually rejects'(this: any) {
+		getGithubSlugStub.returns({ name: 'name', owner: 'owner' });
+		syncStub.returns(Promise.reject());
+
+		const dfd = this.async();
+
+		runGruntTask('sync:latest', dfd.callback((result: any) => {
+			assert.isFalse(result);
+		}));
 	}
 });
-
-// function gruntMock (this: any, task: any) {
-// 	this.task = task;
-// 	this.async = asyncStub;
-
-// 	return this;
-// }
