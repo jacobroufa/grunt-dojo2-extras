bf63c505d50bb16753c490f986ce5436709f87a2
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-05-23 09:35:28 -0500

    begin typedoc integration tests

--- tests/_support/tmpFiles.ts
+++ tests/_support/tmpFiles.ts
@@ -1,11 +1,18 @@
 import { mkdirSync, mkdtempSync, existsSync } from 'fs';
 import { join } from 'path';
 
-export function tmpDirectory(): string {
+export function tmpDirectory(ext?: string): string {
 	if (!existsSync('.test')) {
 		mkdirSync('.test');
 	}
-	return mkdtempSync('.test/dir-');
+
+	const tmpDir = mkdtempSync('.test/dir-');
+
+	if (ext) {
+		return join(tmpDir, `index${ext}`);
+	}
+
+	return tmpDir;
 }
 
 export function tmpFile(name: string = String(Math.floor(Math.random() * 10000))): string {
--- tests/integration/typedoc.ts
+++ tests/integration/typedoc.ts
@@ -5,14 +5,68 @@ import { tmpDirectory } from '../_support/tmpFiles';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
+let sampleRequire: any;
+let outputTarget: any;
+
 registerSuite({
 	name: 'typedoc',
 
-	async build() {
-		const out = tmpDirectory();
+	before() {
+		sampleRequire = (<any> require).toUrl('assets/sample');
+	},
 
-		await typedoc((<any> require).toUrl('assets/sample'), out);
-		const indexFile = readFileSync(join(out, 'index.html'));
+	beforeEach() {
+		outputTarget = tmpDirectory();
+	},
+
+	async build() {
+		await typedoc(sampleRequire, outputTarget);
+
+		const indexFile = readFileSync(join(outputTarget, 'index.html'));
+
 		assert.include(String(indexFile), 'This is a README!');
+	},
+
+	async 'build JSON'() {
+		let json;
+
+		outputTarget = tmpDirectory('.json');
+
+		await typedoc(sampleRequire, outputTarget);
+
+		const indexFile = readFileSync(outputTarget);
+
+		try {
+			json = JSON.parse(String(indexFile));
+		} catch (e) {
+			console.log(e.message);
+			assert.fail('typedoc output should be a JSON object');
+		}
+	},
+
+	async 'test externalPattern and excludeExternals options'() {
+		const opts = {
+			excludeExternals: true,
+			externalPattern: '**/examples/**/*.ts'
+		};
+
+		await typedoc(sampleRequire, outputTarget, opts);
+
+
+	},
+
+	// async excludeOption() {
+	// 	const opts = {
+	// 	};
+
+	// 	await typedoc(sampleRequire, outputTarget, opts);
+	// },
+
+	async 'test includeDeclarations option'() {
+		const opts = {
+			includeDeclarations: true
+		};
+
+		await typedoc(sampleRequire, outputTarget, opts);
 	}
 });
