8cf3ffefe8133af780d8fd0cef59b480f695139e
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-05-24 11:21:49 -0500

    update prebuild tests to mock grunt rather than run it

--- tests/unit/tasks/prebuild.ts
+++ tests/unit/tasks/prebuild.ts
@@ -1,45 +1,65 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
 import * as grunt from 'grunt';
-import { loadTasks, unloadTasks, runGruntTask } from '../../_support/loadGrunt';
+import loadModule, { cleanupModuleMocks } from '../../_support/loadModule';
 import { stub } from 'sinon';
 
+let prebuild: any;
+
+const wrapAsyncTaskStub = stub();
 const decryptDeployKeyStub = stub();
+const registerTaskStub = stub(grunt, 'registerTask');
 
 registerSuite({
 	name: 'tasks/prebuild',
 
-	setup() {
-		grunt.initConfig({});
+	after() {
+		cleanupModuleMocks();
+		registerTaskStub.restore();
+	},
 
-		loadTasks({
+	beforeEach() {
+		prebuild = loadModule('tasks/prebuild', {
+			'./util/wrapAsyncTask': { default: wrapAsyncTaskStub },
 			'../src/commands/decryptDeployKey': { default: decryptDeployKeyStub }
 		});
 	},
 
-	teardown() {
-		unloadTasks();
-	},
-
 	afterEach() {
+		registerTaskStub.reset();
+		wrapAsyncTaskStub.reset();
 		decryptDeployKeyStub.reset();
 	},
 
 	'prebuildTask eventually resolves'(this: any) {
-		decryptDeployKeyStub.returns(Promise.resolve());
-		const dfd = this.async();
-		runGruntTask('prebuild', dfd.callback((result: any) => {
-			assert.isUndefined(result);
-			assert.isTrue(decryptDeployKeyStub.calledOnce);
-		}));
+		wrapAsyncTaskStub.yieldsAsync(this);
+		decryptDeployKeyStub.returns(true);
+
+		try {
+			prebuild(grunt);
+		} catch (e) {
+			// should not throw
+			console.log(e);
+		}
+
+		assert.isTrue(registerTaskStub.calledOnce);
+		assert.isTrue(wrapAsyncTaskStub.calledOnce);
+		assert.isTrue(decryptDeployKeyStub.calledOnce);
 	},
 
 	'prebuildTask eventually rejects'(this: any) {
-		decryptDeployKeyStub.returns(Promise.reject());
-		const dfd = this.async();
-		runGruntTask('prebuild', dfd.callback((result: any) => {
-			assert.isFalse(result);
-			assert.isTrue(decryptDeployKeyStub.calledOnce);
-		}));
+		wrapAsyncTaskStub.yieldsAsync(this);
+		decryptDeployKeyStub.returns(false);
+
+		try {
+			prebuild(grunt);
+		} catch (e) {
+			// might throw
+			console.log(e);
+		}
+
+		assert.isTrue(registerTaskStub.calledOnce);
+		assert.isTrue(wrapAsyncTaskStub.calledOnce);
+		assert.isTrue(decryptDeployKeyStub.calledOnce);
 	}
 });
