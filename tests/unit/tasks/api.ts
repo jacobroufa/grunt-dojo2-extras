8cf89598555bc1465d11e59904f1df825a67613b
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-05-25 15:21:18 -0500

    ensure `tasks/api` tests do not run grunt

--- tests/unit/tasks/api.ts
+++ tests/unit/tasks/api.ts
@@ -1,11 +1,11 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
 import * as grunt from 'grunt';
-import { loadTasks, unloadTasks, runGruntTask } from '../../_support/loadGrunt';
-import { stub, spy, SinonSpy } from 'sinon';
+import { stub, spy } from 'sinon';
+import loadModule, { cleanupModuleMocks } from '../../_support/loadModule';
+import { setupWrappedAsyncStub } from '../../_support/tasks';
 
-let GitHub: any;
-let GitHubSpy: SinonSpy;
+let api: any;
 
 const typedocStub = stub();
 const syncStub = stub();
@@ -20,52 +20,27 @@ const joinStub = stub();
 const resolveStub = stub();
 const installDependenciesStub = stub();
 const makeTempDirectoryStub = stub();
+const wrapAsyncTaskStub = stub();
+const optionsStub = stub();
+const registerMultiTaskStub = stub(grunt, 'registerMultiTask');
+const GitHub = class {
+	name: string;
+	url: string;
+	constructor(owner: any, name: any) {
+		this.name = name;
+		this.url = `https://github.com/${owner}/${name}`;
+		return this;
+	}
+};
+const GitHubSpy = spy(GitHub);
 
 registerSuite({
 	name: 'tasks/api',
 
-	setup() {
-		grunt.initConfig({
-			api: {
-				options: {
-					format: 'html',
-					typedoc: {
-						mode: 'file',
-						excludeExternals: true,
-						excludeNotExported: true,
-						ignoreCompilerErrors: true
-					}
-				},
-				html: {
-					options: {
-						dest: '<%= apiDirectory %>',
-						src: '.'
-					}
-				},
-				json: {
-					options: {
-						format: 'json',
-						dest: '<%= apiDirectory %>/api.json',
-						src: '.'
-					}
-				}
-			}
-		});
-
-		GitHub = class {
-			name: string;
-			url: string;
-			constructor(owner: any, name: any) {
-				this.name = name;
-				this.url = `https://github.com/${owner}/${name}`;
-				return this;
-			}
-		};
-
-		GitHubSpy = spy(GitHub);
-
-		loadTasks({
+	beforeEach() {
+		api = loadModule('tasks/api', {
 			'../src/commands/typedoc': { default: typedocStub },
+			'./util/wrapAsyncTask': { default: wrapAsyncTaskStub },
 			'../src/util/GitHub': { default: GitHubSpy },
 			'../src/commands/sync': { default: syncStub },
 			'../src/commands/getReleases': {
@@ -86,8 +61,9 @@ registerSuite({
 		});
 	},
 
-	teardown() {
-		unloadTasks();
+	after() {
+		cleanupModuleMocks();
+		registerMultiTaskStub.restore();
 	},
 
 	afterEach() {
@@ -105,53 +81,226 @@ registerSuite({
 		resolveStub.reset();
 		installDependenciesStub.reset();
 		makeTempDirectoryStub.reset();
+		wrapAsyncTaskStub.reset();
+		optionsStub.reset();
+		registerMultiTaskStub.reset();
 	},
 
-	'api task has remote options; eventually resolves'(this: any) {
-		const dfd = this.async();
+	'api task has remote options including html format and string repo; no missing filters, no APIs match; eventually resolves'(this: any) {
+		getReleasesStub.returns([]);
+		optionsStub.returns({
+			src: 'src',
+			dest: 'dest',
+			format: 'html',
+			repo: 'user/repo',
+			cloneDirectory: 'cloneDirectory',
+			filter: 'filter',
+			skipInstall: true,
+			typedoc: {}
+		});
 
-		runGruntTask('api', dfd.callback((result: any) => {
-			assert.isUndefined(result);
-		}));
+		setupWrappedAsyncStub.call({
+			options: optionsStub
+		}, wrapAsyncTaskStub, this.async(), () => {
+			console.log('asserting things about remote opts test');
+			assert.isTrue(optionsStub.calledOnce);
+			assert.isTrue(GitHubSpy.calledOnce);
+			assert.isTrue(joinStub.notCalled);
+			assert.isTrue(makeTempDirectoryStub.notCalled);
+			assert.isTrue(createVersionFilterStub.calledOnce);
+			assert.isTrue(createHtmlApiMissingFilterStub.calledOnce);
+			assert.isTrue(getReleasesStub.calledOnce);
+			assert.isTrue(getHtmlApiPathStub.notCalled);
+		});
+
+		api(grunt);
+
+		assert.isTrue(wrapAsyncTaskStub.calledOnce);
+		assert.isTrue(registerMultiTaskStub.calledOnce);
 	},
 
-	'api task has remote options; no APIs match the filter; eventually resolves'(this: any) {
-		const dfd = this.async();
+	'api task has remote options including json format and object repo; no filters, all APIs up to date; eventually resolves'(this: any) {
+		getReleasesStub.returns([]);
+		optionsStub.returns({
+			src: 'src',
+			dest: 'dest',
+			format: 'json',
+			repo: { owner: 'user', name: 'repo' },
+			skipInstall: true,
+			typedoc: {}
+		});
 
-		runGruntTask('api', dfd.callback((result: any) => {
-			assert.isUndefined(result);
-		}));
+		setupWrappedAsyncStub.call({
+			options: optionsStub
+		}, wrapAsyncTaskStub, this.async(), () => {
+			console.log('asserting things about remote opts test');
+			assert.isTrue(optionsStub.calledOnce);
+			assert.isTrue(GitHubSpy.calledOnce);
+			assert.isTrue(joinStub.calledOnce);
+			assert.isTrue(makeTempDirectoryStub.calledOnce);
+			assert.isTrue(createVersionFilterStub.notCalled);
+			assert.isTrue(createJsonApiMissingFilterStub.calledOnce);
+			assert.isTrue(getReleasesStub.calledOnce);
+			assert.isTrue(getJsonApiPathStub.notCalled);
+		});
+
+		api(grunt);
+
+		assert.isTrue(wrapAsyncTaskStub.calledOnce);
+		assert.isTrue(registerMultiTaskStub.calledOnce);
 	},
 
-	'api task has remote options; all APIs are up to date; eventually resolves'(this: any) {
-		const dfd = this.async();
+	'api task has remote options including json format and object repo; latest filters, all APIs up to date; eventually resolves'(this: any) {
+		getReleasesStub.returns([{ name: 'name' }]);
+		optionsStub.returns({
+			src: 'src',
+			dest: 'dest',
+			format: 'json',
+			repo: { owner: 'user', name: 'repo' },
+			filter: 'latest',
+			skipInstall: true,
+			typedoc: {}
+		});
 
-		runGruntTask('api', dfd.callback((result: any) => {
-			assert.isUndefined(result);
-		}));
+		setupWrappedAsyncStub.call({
+			options: optionsStub
+		}, wrapAsyncTaskStub, this.async(), () => {
+			console.log('asserting things about remote opts test');
+			assert.isTrue(optionsStub.calledOnce);
+			assert.isTrue(GitHubSpy.calledOnce);
+			assert.isTrue(joinStub.calledOnce);
+			assert.isTrue(makeTempDirectoryStub.calledOnce);
+			assert.isTrue(createVersionFilterStub.notCalled);
+			assert.isTrue(createJsonApiMissingFilterStub.calledOnce);
+			assert.isTrue(getReleasesStub.calledOnce);
+			assert.isTrue(getJsonApiPathStub.calledOnce);
+			assert.isTrue(syncStub.calledOnce);
+			assert.isTrue(installDependenciesStub.notCalled);
+			assert.isTrue(typedocStub.calledOnce);
+		});
+
+		api(grunt);
+
+		assert.isTrue(wrapAsyncTaskStub.calledOnce);
+		assert.isTrue(registerMultiTaskStub.calledOnce);
+	},
+
+	'api task has remote options including filter object; runs installDependencies; eventually resolves'(this: any) {
+		getReleasesStub.returns([{ name: 'name' }]);
+		optionsStub.returns({
+			src: 'src',
+			dest: 'dest',
+			format: 'html',
+			repo: { owner: 'user', name: 'repo' },
+			filter: {},
+			skipInstall: false,
+			typedoc: {}
+		});
+
+		setupWrappedAsyncStub.call({
+			options: optionsStub
+		}, wrapAsyncTaskStub, this.async(), () => {
+			console.log('asserting things about remote opts test');
+			assert.isTrue(optionsStub.calledOnce);
+			assert.isTrue(GitHubSpy.calledOnce);
+			assert.isTrue(joinStub.calledOnce);
+			assert.isTrue(makeTempDirectoryStub.calledOnce);
+			assert.isTrue(createVersionFilterStub.notCalled);
+			assert.isTrue(createHtmlApiMissingFilterStub.calledOnce);
+			assert.isTrue(getReleasesStub.calledOnce);
+			assert.isTrue(getHtmlApiPathStub.calledOnce);
+			assert.isTrue(syncStub.calledOnce);
+			assert.isTrue(installDependenciesStub.calledOnce);
+			assert.isTrue(typedocStub.calledOnce);
+		});
+
+		api(grunt);
+
+		assert.isTrue(wrapAsyncTaskStub.calledOnce);
+		assert.isTrue(registerMultiTaskStub.calledOnce);
 	},
 
 	'api task has remote options; runs installDependencies; eventually resolves'(this: any) {
-		const dfd = this.async();
+		getReleasesStub.returns([{ name: 'name' }]);
+		optionsStub.returns({
+			src: 'src',
+			dest: 'dest',
+			format: 'html',
+			repo: { owner: 'user', name: 'repo' },
+			filter: [],
+			skipInstall: false,
+			typedoc: {}
+		});
 
-		runGruntTask('api', dfd.callback((result: any) => {
-			assert.isUndefined(result);
-		}));
+		setupWrappedAsyncStub.call({
+			options: optionsStub
+		}, wrapAsyncTaskStub, this.async(), () => {
+			console.log('asserting things about remote opts test');
+			assert.isTrue(optionsStub.calledOnce);
+			assert.isTrue(GitHubSpy.calledOnce);
+			assert.isTrue(joinStub.calledOnce);
+			assert.isTrue(makeTempDirectoryStub.calledOnce);
+			assert.isTrue(createVersionFilterStub.notCalled);
+			assert.isTrue(createHtmlApiMissingFilterStub.calledOnce);
+			assert.isTrue(getReleasesStub.calledOnce);
+			assert.isTrue(getHtmlApiPathStub.calledOnce);
+			assert.isTrue(syncStub.calledOnce);
+			assert.isTrue(installDependenciesStub.calledOnce);
+			assert.isTrue(typedocStub.calledOnce);
+		});
+
+		api(grunt);
+
+		assert.isTrue(wrapAsyncTaskStub.calledOnce);
+		assert.isTrue(registerMultiTaskStub.calledOnce);
 	},
 
 	'api task has no remote options; eventually resolves'(this: any) {
-		const dfd = this.async();
+		optionsStub.returns({
+			src: 'src',
+			dest: 'dest',
+			format: 'html',
+			skipInstall: true,
+			typedoc: {}
+		});
 
-		runGruntTask('api', dfd.callback((result: any) => {
-			assert.isUndefined(result);
-		}));
+		setupWrappedAsyncStub.call({
+			options: optionsStub
+		}, wrapAsyncTaskStub, this.async(), () => {
+			console.log('asserting things about no remote opts test');
+			assert.isTrue(optionsStub.calledOnce);
+			assert.isTrue(resolveStub.calledOnce);
+			assert.isTrue(typedocStub.calledOnce);
+		});
+
+		api(grunt);
+
+		assert.isTrue(wrapAsyncTaskStub.calledOnce);
+		assert.isTrue(registerMultiTaskStub.calledOnce);
 	},
 
 	'api task has no remote options; runs installDependencies; eventually resolves'(this: any) {
-		const dfd = this.async();
+		optionsStub.returns({
+			src: 'src',
+			dest: 'dest',
+			format: 'html',
+			skipInstall: false,
+			typedoc: {}
+		});
 
-		runGruntTask('api', dfd.callback((result: any) => {
-			assert.isUndefined(result);
-		}));
+		setupWrappedAsyncStub.call({
+			options: optionsStub
+		}, wrapAsyncTaskStub, this.async(), () => {
+			console.log('asserting things about no remote opts test, should installDeps');
+			assert.isTrue(optionsStub.calledOnce);
+			assert.isTrue(installDependenciesStub.calledOnce);
+			assert.isTrue(resolveStub.calledOnce);
+			assert.isTrue(typedocStub.calledOnce);
+		});
+
+		api(grunt);
+
+		assert.isTrue(wrapAsyncTaskStub.calledOnce);
+		assert.isTrue(registerMultiTaskStub.calledOnce);
 	}
 });
